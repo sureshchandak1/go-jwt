fa88ddd3e44f4e9ab7028d9907ca5815942c1009
SSureshC <sureshchandak1@gmail.com>
2024-12-12 19:51:49 +0530

    Setup .env and Postgres DB

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sureshchandak1/go-jwt/initializers"
+)
 
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDb()
+	initializers.SyncDatabase()
+}
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run()
 }
